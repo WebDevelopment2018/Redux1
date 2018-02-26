ed976f3260f613edb4da2d359047b5ac36653bf6
KaterynaMohylevska <mohylevska@ucu.edu.ua>
2018-02-26 15:36:19 +0200

    Avoiding Object Mutations with Object.assign() and ...spread

--- Kateryna/src/tests.js
+++ Kateryna/src/tests.js
@@ -25,4 +25,38 @@ expect (
     counter(undefined, {})
 ).toEqual(0);
 
+const deepFreeze = require('deep-freeze');
+const testAddCounter = () => {
+    const listBefore = [];
+    const listAfter = [0];
+
+    deepFreeze(listBefore);
+
+    expect(
+        addCounter(listBefore)
+    ).toEqual(listAfter);
+};
+const testRemoveCounter = () => {
+    const listBefore = [0, 10, 20];
+    const listAfter = [0, 20];
+    deepFreeze(listBefore);
+    expect(
+        removeCounter(listBefore, 1)
+    ).toEqual(listAfter);
+};
+const testIncrementCounter = () => {
+    const listBefore = [0, 10, 20];
+    const listAfter = [0, 11, 20];
+
+    deepFreeze(listBefore);
+
+    expect(
+        incrementCounter(listBefore, 1)
+    ).toEqual(listAfter);
+};
+
+testAddCounter();
+testRemoveCounter();
+testIncrementCounter();
+
 console.log("Tests passed!");
