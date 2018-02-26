412b3ed1d923abe612bb83eb6af2845ed6e7f393
KaterynaMohylevska <mohylevska@ucu.edu.ua>
2018-02-26 18:13:08 +0200

    Writing a Todo List Reducer (Toggling a Todo)

--- Kateryna/src/app.js
+++ Kateryna/src/app.js
@@ -4,6 +4,7 @@ import expect from 'expect';
 import ReactDOM from 'react-dom';
 
 const deepFreeze = require('deep-freeze');
+
 const todos = (state = [], action) => {
     switch (action.type) {
         case 'ADD_TODO':
@@ -15,6 +16,17 @@ const todos = (state = [], action) => {
                     completed: false
                 }
             ];
+        case 'TOGGLE_TODO':
+            return state.map(todo => {
+                if (todo.id !== action.id) {
+                    return todo;
+                } else {
+                    return {
+                        ...todo,
+                        completed: !todo.completed
+                    };
+                }
+            });
         default:
             return state;
     }
@@ -41,5 +53,45 @@ const testAddTodo = () => {
     ).toEqual(stateAfter);
 };
 
+const testToggleTodo = () => {
+    const stateBefore = [
+        {
+            id: 0,
+            text: 'Learn Redux',
+            completed: false
+        },
+        {
+            id: 1,
+            text: 'Go Shopping',
+            completed: false
+        }
+    ];
+    const action = {
+        type: 'TOGGLE_TODO',
+        id: 1
+    };
+
+    const stateAfter = [
+        {
+            id: 0,
+            text: 'Learn Redux',
+            completed: false
+        },
+        {
+            id: 1,
+            text: 'Go Shopping',
+            completed: true
+        }
+    ];
+
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+
+    expect(
+        todos(stateBefore, action)
+    ).toEqual(stateAfter);
+};
+
 testAddTodo();
+testToggleTodo();
 console.log('All tests passed')
