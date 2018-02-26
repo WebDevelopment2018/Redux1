b8656d66bbd780c203bb4bca50da253d04acc287
KaterynaMohylevska <mohylevska@ucu.edu.ua>
2018-02-26 15:21:48 +0200

    Avoiding Array Mutations

--- Kateryna/src/app.js
+++ Kateryna/src/app.js
@@ -1,7 +1,65 @@
 import {createStore} from 'redux';
 import React from "react"
+import expect from 'expect';
 import ReactDOM from 'react-dom';
 
+const deepFreeze = require('deep-freeze');
+
+const addCounter = (list) => [...list, 0];
+// return list.concat([0]); // old way
+
+const removeCounter = (list, index) => [
+    ...list.slice(0, index),
+    ...list.slice(index + 1)
+]
+// Old way:
+//return list
+//  .slice(0, index)
+//  .concat(list.slice(index + 1));
+
+const incrementCounter = (list, index) => [
+    ...list.slice(0, index),
+    list[index] + 1,
+    ...list.slice(index + 1)
+];
+
+
+const testAddCounter = () => {
+    const listBefore = [];
+    const listAfter = [0];
+
+    deepFreeze(listBefore);
+
+    expect(
+        addCounter(listBefore)
+    ).toEqual(listAfter);
+};
+const testRemoveCounter = () => {
+    const listBefore = [0, 10, 20];
+    const listAfter = [0, 20];
+    deepFreeze(listBefore);
+    expect(
+        removeCounter(listBefore, 1)
+    ).toEqual(listAfter);
+};
+const testIncrementCounter = () => {
+    const listBefore = [0, 10, 20];
+    const listAfter = [0, 11, 20];
+
+    deepFreeze(listBefore);
+
+    expect(
+        incrementCounter(listBefore, 1)
+    ).toEqual(listAfter);
+};
+
+testAddCounter();
+testRemoveCounter();
+testIncrementCounter();
+console.log('All tests passed');
+
+
+/*
 const counter = (state = 0, action) => {
     switch (action.type) {
         case 'INCREMENT':
@@ -49,3 +107,4 @@ const render = () => {
 
 store.subscribe(render);
 render();
+*/
