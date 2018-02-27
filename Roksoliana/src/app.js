c1372a0df421c8b311bf3598fc6efee000c6014c
RoksolyanaLebedenko <lebedenko@ucu.edu.ua>
2018-02-27 17:16:31 +0200

    Writing a Todo List Reducer (Toggling a Todo).

--- Roksoliana/src/app.js
+++ Roksoliana/src/app.js
@@ -15,6 +15,16 @@ const todos = (state =[], action) => {
                     completed:false
                 }
             ];
+        case 'TOGGLE_TODO':
+            return state.map(todo => {
+                if (todo.id !== action.id){
+                    return todo;
+                }
+                return Object.assign({}, todo, {
+                    completed: !todo.completed
+                });
+            });
+
         default:
             return state;
     }
@@ -32,6 +42,43 @@ const testAddTodo = () => {
             id: 0,
             text: 'Learn Redux',
             completed: false
+        }
+    ];
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+
+    expect(
+        todos(stateBefore,action)
+    ).toEqual(stateAfter);
+};
+
+const testToggleTodo = () => {
+    const stateBefore = [
+        {
+            id: 0,
+            text: 'Learn Redux',
+            completed: false
+        },
+        {
+            id: 1,
+            text: 'Go shopping',
+            completed: false
+        }
+    ];
+    const action = {
+        type: 'TOGGLE_TODO',
+        id: 1
+    };
+    const stateAfter = [
+        {
+            id: 0,
+            text: 'Learn Redux',
+            completed: false
+        },
+        {
+            id: 1,
+            text: 'Go shopping',
+            completed: true
         }
     ];
     deepFreeze(stateBefore);
