455925f23c8a39466058fd2870d5ae57ea90724f
RoksolyanaLebedenko <lebedenko@ucu.edu.ua>
2018-02-27 16:56:15 +0200

    Avoiding Object Mutations with Object.assign() and ...spread.

--- Roksoliana/src/app.js
+++ Roksoliana/src/app.js
@@ -3,53 +3,32 @@ import React from "react";
 import expect from 'expect';
 let deepFreeze = require('deepfreeze');
 
-const addCounter = (list) => {
-    return [...list, 0];
+
+const toggleTodo = (todo) => {
+    return Object.assign({}, todo, {
+        //...todo
+        completed: !todo.completed
+    });
 };
 
-const removeCounter = (list, index) => [
-    ...list.slice(0, index),
-    ...list.slice(index + 1)
-];
+const testToggleTodo = () => {
+    const todoBefore = {
+        id: 0,
+        text: 'Learn Redux',
+        completed: false
+    };
+    const todoAfter = {
+        id: 0,
+        text: 'Learn Redux',
+        completed: true
+    };
 
-const incrementCounter = (list, index) => [
-    ...list.slice(0, index),
-    list[index] + 1,
-    ...list.slice(index + 1)
-];
-
-const testAddCounter = () => {
-    const listBefore = [];
-    const listAfter = [0];
-
-    deepFreeze(listBefore);
+    deepFreeze(todoBefore);
 
     expect(
-        addCounter(listBefore)
-    ).toEqual(listAfter);
-};
-const testRemoveCounter = () => {
-    const listBefore = [0, 10, 20];
-    const listAfter = [0, 20];
-
-    deepFreeze(listBefore);
-
-    expect(
-        removeCounter(listBefore, 1)
-    ).toEqual(listAfter);
-};
-const testIncrementCounter = () => {
-    const listBefore = [0, 10, 20];
-    const listAfter = [0, 11, 20];
-
-    deepFreeze(listBefore);
-
-    expect(
-        incrementCounter(listBefore, 1)
-    ).toEqual(listAfter);
+        toggleTodo(todoBefore)
+    ).toEqual(todoAfter);
 };
 
-testAddCounter();
-testRemoveCounter();
-testIncrementCounter();
-console.log('All tests passed');
+testToggleTodo();
+console.log('All tests passed.');
