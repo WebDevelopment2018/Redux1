0eeb1551d59b3b7a93853f0df1f67149d83708b7
KaterynaMohylevska <mohylevska@ucu.edu.ua>
2018-03-05 16:49:02 +0200

    Using mapDispatchToProps() Shorthand Notation

--- Kateryna/src/components/VisibleTodoList.js
+++ Kateryna/src/components/VisibleTodoList.js
@@ -22,15 +22,15 @@ const getVisibleTodos = (todos, filter) => {
 const mapStateToProps = (state, {  match }) => ({
     todos: getVisibleTodos(state.todos, match.params.filter || 'all'),
 });
-const mapDispatchToProps = (dispatch) => ({
-    onTodoClick: (id) => {
-        dispatch(toggleTodo(id));
-    }
-});
+// const mapDispatchToProps = (dispatch) => ({
+//     onTodoClick: (id) => {
+//         dispatch(toggleTodo(id));
+//     }
+// });
 
 const VisibleTodoList = withRouter(connect(
     mapStateToProps,
-    mapDispatchToProps
+    { onTodoClick: toggleTodo }
 )(TodoList));
 
 
