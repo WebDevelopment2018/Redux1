85d060b8c648b295dfe10e5666bb884181122685
KaterynaMohylevska <mohylevska@ucu.edu.ua>
2018-03-07 15:39:18 +0200

    Normalizing API Responses with normalizer

--- Kateryna/src/actions/index.js
+++ Kateryna/src/actions/index.js
@@ -1,3 +1,6 @@
+import {normalize} from 'normalizr';
+
+import* as schema from './schema';
 import * as api from '../api';
 import {getIsFetching} from '../reducers';
 
@@ -6,7 +9,7 @@ export const addTodo = (text) => (dispatch) =>
     api.addTodo(text).then(response => {
         dispatch({
             type: 'ADD_TODO_SUCCESS',
-            response,
+            response: normalize(response, schema.todo),
         });
     });
 
@@ -25,7 +28,7 @@ export const fetchTodos = (filter) => (dispatch, getState) => {
             dispatch({
                 type: 'FETCH_TODOS_SUCCESS',
                 filter,
-                response,
+                response:normalize(response, schema.arrayOfTodos),
             });
         },
         error => {
